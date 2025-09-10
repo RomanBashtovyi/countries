097b9349b931b09efab4c5558605101b5b53dbb7
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-10 10:03:51 +0300

    Set up Redux store and integrate with React application

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+
+import { rootReducer } from './root-reducer'
+import * as api from '../config'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk.withExtraArgument({ client: axios, api })))
+)
+
+export default store
