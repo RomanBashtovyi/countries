b24c2df6c981899afd3e4c9759de1acdbbc10fd0
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-10 18:28:36 +0300

    Implement search functionality with Redux integration in Search component and update HomePage to filter countries based on search input

--- src/store/countries/countries-selectors.js
+++ src/store/countries/countries-selectors.js
@@ -4,3 +4,6 @@ export const selectCountriesInfo = (state) => ({
   qty: state.countries.list.length,
 })
 export const selectAllCountries = (state) => state.countries.list
+export const selectVisibleCountries = (state, { search = '' }) => {
+  return state.countries.list.filter((country) => country.name.toLowerCase().includes(search.toLowerCase()))
+}
