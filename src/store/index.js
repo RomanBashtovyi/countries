f6c90bfef84fc5be80004dba4e59baac9588aeb7
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-11 11:44:45 +0300

    Add theme persistence

--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,33 @@ import * as api from '../config'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const loadTheme = () => {
+  try {
+    const saved = localStorage.getItem('theme')
+    return saved ? saved : undefined
+  } catch (_) {
+    return undefined
+  }
+}
+
+const preloadedState = {}
+const savedTheme = loadTheme()
+if (savedTheme) {
+  preloadedState.theme = savedTheme
+}
+
 const store = createStore(
   rootReducer,
+  preloadedState,
   composeEnhancers(applyMiddleware(thunk.withExtraArgument({ client: axios, api })))
 )
+
+store.subscribe(() => {
+  try {
+    const state = store.getState()
+    const currentTheme = state.theme
+    localStorage.setItem('theme', currentTheme)
+  } catch (_) {}
+})
 
 export default store
