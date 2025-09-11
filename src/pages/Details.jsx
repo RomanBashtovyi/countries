5033aff85d2c99e2d385053332a8f30faf0aea87
Roman Bashtovyi <roman.bashtovyy@gmail.com>
2025-09-11 11:41:58 +0300

    Add details management with Redux: implement actions, reducer, and selectors for country details and neighbors; update Info and Details components for data fetching and rendering

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,22 +1,36 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { IoArrowBack } from 'react-icons/io5';
+import { useNavigate, useParams } from 'react-router-dom'
+import { IoArrowBack } from 'react-icons/io5'
 
-import { Button } from '../components/Button';
-import { Info } from '../components/Info';
+import { Button } from '../components/Button'
+import { Info } from '../components/Info'
+import { selectDetails } from '../store/details/details-selector'
+import { loadCountryByName, clearDetails } from '../store/details/details-actions'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 export const Details = () => {
-  const { name } = useParams();
-  const navigate = useNavigate();
+  const { name } = useParams()
+  const navigate = useNavigate()
 
-  const currentCountry = null;
+  const dispatch = useDispatch()
+  const { currentCountry, status, error } = useSelector(selectDetails)
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name))
+    return () => {
+      dispatch(clearDetails())
+    }
+  }, [name, dispatch])
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
+      {status === 'loading' && <h2>Loading...</h2>}
+      {error && <h2>Can't fetch data</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
-  );
-};
+  )
+}
---
+++ src/store/details/details-actions.js
@@ -0,0 +1,48 @@
+export const SET_LOADING = '@@details/SET_LOADING'
+export const SET_ERROR = '@@details/SET_ERROR'
+export const SET_COUNTRY = '@@details/SET_COUNTRY'
+export const CLEAR_DETAILS = '@@details/CLEAR_DETAILS'
+export const SET_NEIGHBORS = '@@details/SET_NEIGHBORS'
+
+const setLoading = (loading) => ({
+  type: SET_LOADING,
+})
+
+const setError = (error) => ({
+  type: SET_ERROR,
+  payload: error,
+})
+
+const setCountry = (country) => ({
+  type: SET_COUNTRY,
+  payload: country,
+})
+
+export const clearDetails = () => ({
+  type: CLEAR_DETAILS,
+})
+
+const setNeighbors = (countryNames) => ({
+  type: SET_NEIGHBORS,
+  payload: countryNames,
+})
+
+export const loadCountryByName =
+  (name) =>
+  (dispatch, _, { client, api }) => {
+    dispatch(setLoading())
+
+    client
+      .get(api.searchByCountry(name))
+      .then(({ data }) => dispatch(setCountry(data[0])))
+      .catch((err) => dispatch(setError(err.message)))
+  }
+
+export const loadNeighborsByBorder =
+  (borders) =>
+  (dispatch, _, { client, api }) => {
+    client
+      .get(api.filterByCode(borders))
+      .then(({ data }) => dispatch(setNeighbors(data.map((country) => country.name))))
+      .catch((err) => dispatch(setError(err.message)))
+  }
